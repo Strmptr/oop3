cdcbc9c88243d0c1a24d5767c9db094c9b6ce6d8
Strmptr <alekseev2013andrey@yandex.ru>
2020-04-28 13:06:46 +0700

    Добавьте файлы проекта.

---
+++ Lab3/Classes/Human.cs
@@ -0,0 +1,60 @@
+﻿using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Device.Location;
+
+namespace Lab3
+{
+    class Human : MapObject
+    {
+        PointLatLng point = new PointLatLng();
+
+        public Human(string name, PointLatLng Point) : base(name)
+        {
+            this.point = Point;
+        }
+
+        public override double getDistance(PointLatLng pointtwo)
+        {
+            GeoCoordinate geo1 = new GeoCoordinate(point.Lat, point.Lng);
+            GeoCoordinate geo2 = new GeoCoordinate(pointtwo.Lat, pointtwo.Lng);
+            return geo1.GetDistanceTo(geo2);
+        }
+
+        public override PointLatLng getFocus()
+        {
+            return point;
+        }
+
+        public override string getTitle()
+        {
+            return objectName;
+        }
+        public override DateTime getCreationDate()
+        {
+            return creationTime;
+        }
+        public override GMapMarker GetMarker()
+        {
+            GMapMarker marker = new GMapMarker(point)
+            {
+                Shape = new Image
+                {
+                    Width = 32,
+                    Height = 32,
+                    ToolTip = "Человек",
+                    Source = new BitmapImage(new Uri("pack://application:,,,/Resources/men.png"))
+                }
+            };
+            return marker;
+        }
+
+        
+    }
+}
